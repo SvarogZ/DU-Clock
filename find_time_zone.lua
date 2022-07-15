1b9615225ed3ad9f0d8a5b3c8a69e1fddabf29aa
Svarog <alexander.s.zernov@gmail.com>
2022-07-15 19:33:08 +1200

    Create find_time_zone.lua

---
+++ find_time_zone.lua
@@ -0,0 +1,15 @@
+-- works with any dynamic construct
+local worldWest = {0,1,0}
+local worldVertical = core.getWorldVertical()
+
+local worldVerticalVec = -vec3(worldVertical)
+local worldWestVec = vec3(worldWest)
+
+local function angle_between(vector1,vector2)
+    return math.acos(utils.clamp(vector1:dot(vector2) / (vector1:len() * vector2:len()),-1,1))
+end
+
+local angle = angle_between(worldVerticalVec,worldWestVec)
+local angleClock = angle / math.pi * 24
+
+system.print(angleClock)
