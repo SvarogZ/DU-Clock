72690b7a2c73f48f27c7f7305cac9a5a75964135
Svarog <alexander.s.zernov@gmail.com>
2022-07-15 21:24:24 +1200

    Update find_time_zone.lua

--- find_time_zone.lua
+++ find_time_zone.lua
@@ -1,10 +1,12 @@
 -- Script helps to find the time offset on the surface of a planet
 -- works with any dynamic construct
+local worldNorth = {0,0,1}
 local worldWest = {0,1,0}
 local worldVertical = core.getWorldVertical()
 
 local worldVerticalVec = -vec3(worldVertical)
 local worldWestVec = vec3(worldWest)
+local worldNorthVec = vec3(worldNorth)
 
 local function angle_between(vector1,vector2)
     return math.acos(utils.clamp(vector1:dot(vector2) / (vector1:len() * vector2:len()),-1,1))
@@ -13,4 +15,10 @@ end
 local angle = angle_between(worldVerticalVec,worldWestVec)
 local angleClock = angle / math.pi * 24
 
-system.print(angleClock)
+local sign = worldNorthVec:cross(worldWestVec):dot(worldVerticalVec)
+
+if sign > 0 then
+	system.print(angleClock)
+else
+	system.print(-angleClock)
+end
